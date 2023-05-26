c3cea2f9ea3891e7664f57c34e2408382b5fb27f
Harold <hhausman@gmail.com>
2023-05-26 15:04:36 -0600

    `[40]` Attempt clojars scm connection

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
   (:refer-clojure :exclude [compile]))
 
 (def lib 'com.techascent/tmd-parquet)
-(def version "1.000-beta-39")
+(def version "1.000-beta-40")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s.jar" (name lib)))
@@ -17,7 +17,8 @@
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]})
+                :src-dirs ["src"]
+                :scm {:url "https://github.com/techascent/tech.parquet"}})
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
